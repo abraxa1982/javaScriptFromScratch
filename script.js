825f6b835e4ac4605cc7dbe03145548bf85c9f0a
Meike Ziesecke <m.ziesecke@travello.de>
2023-03-14 07:34:20 +0100

    Values and Variables

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let js = 'amazing';
-
-if(js === 'amazing') alert('JavaScript is Fun');
-
 console.log(40+8+23-10);
+
+let firstName = 'Meike';
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myParentsJob = 'some other';
+
+// let 3years = 3 - nicht erlaubt - wie überall, Dollarzeichen sind aber erlaubt
+// name ist erlaubt, aber kann zu Fehlern führen
+
+
