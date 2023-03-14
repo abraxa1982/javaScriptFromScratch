9ee3a0f9e1d71588cf6ae4365b0a8effd26cf6fd
Meike Ziesecke <m.ziesecke@travello.de>
2023-03-14 08:07:14 +0100

    DataTypes

--- assignment.js
+++ assignment.js
@@ -5,3 +5,13 @@ let population = 84;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+/*
 let js = 'amazing';
-console.log(40+8+23-10);
+// console.log(40+8+23-10);
 
 let firstName = 'Meike';
 console.log(firstName);
 
+//Konstante
 let PI = 3.1415;
 
 let myFirstJob = 'Programmer';
 let myParentsJob = 'some other';
+*/
 
 // let 3years = 3 - nicht erlaubt - wie überall, Dollarzeichen sind aber erlaubt
 // name ist erlaubt, aber kann zu Fehlern führen
 
+// Primitive Datentypen: Number (immer float quasi), String, Boolean, Undefined (Deklariert aber nicht befüllt),
+// Null (wie immer), Symbol (unique und nicht änderbar) und BigInt
 
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+// undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+// null
+console.log(typeof null);
