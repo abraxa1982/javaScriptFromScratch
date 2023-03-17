01b823f63023c29f58c8baa8ab7ae50444ec7285
Meike Ziesecke <m.ziesecke@travello.de>
2023-03-17 08:04:53 +0100

    Control Structures if-else

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const firstname = "Meike";
 const job = "developer";
 const birthYear = 1982;
@@ -16,3 +17,27 @@ console.log(`Another
 multiline
 String`);
 
+*/
+const age = 15;
+
+const requiredAge = 18;
+const isOldEnough = age >= requiredAge;
+
+if( age >= requiredAge) {
+    console.log('Sarah can start driving license.');
+} else {
+    let yearsLeft = requiredAge - age;
+    console.log(`Sarah is to young, she has to wait another ${yearsLeft} years.`)
+}
+
+let birthYear = 1982;
+let century; // muss außerhalb des codeblocks definiert werden, sonst ist es außerhalb des codeblocks nicht
+             // zugreifbar, auch wenn es in beiden Schleifen definiert ist.
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
