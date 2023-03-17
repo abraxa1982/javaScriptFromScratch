a579fa4e585e6d898814c69c23f8f89acbf35dd1
Meike Ziesecke <m.ziesecke@travello.de>
2023-03-17 08:26:15 +0100

    Coding Challene Number 2

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-/*
-const firstname = "Meike";
-const job = "developer";
-const birthYear = 1982;
-const year = 2023;
+// Coding Challenge #1
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+//     BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+//     1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+//     Test data:
+//     § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+//     GOOD LUCK 😀
 
-const meike = "I'm " + firstname + ", a " + (year - birthYear) + " years old " + job + ".";
-console.log(meike);
+// Coding Challenge #2
+// Use the BMI example from Challenge #1, and the code you already wrote, and
+// improve it.
+//     Your tasks:
+//     1. Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!"
+// Hint: Use an if/else statement 😉
+//     GOOD LUCK 😀
 
-//muss mit `` umschlossen werden, nennt sich template String
-const meikeNew = `I'm ${firstname}, a ${year - birthYear} years old ${job}.`;
-console.log(meikeNew)
 
-console.log('String with \n\ multiple \n\ lines');
+const markHeight = 1.69;
+const markWeight = 128;
+const johnHeight = 1.95;
+const johnWeight = 92;
 
-console.log(`Another
-multiline
-String`);
+let markBMI = markWeight / markHeight**2;
+let johnBMI = johnWeight / johnHeight**2;
 
-*/
-const age = 15;
-
-const requiredAge = 18;
-const isOldEnough = age >= requiredAge;
-
-if( age >= requiredAge) {
-    console.log('Sarah can start driving license.');
+if (johnBMI > markBMI) {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's! (${markBMI})`);
+} else if(markBMI > johnBMI) {
+    console.log(`Marks's BMI (${markBMI}) is higher than Johns's(${johnBMI})!`);
 } else {
-    let yearsLeft = requiredAge - age;
-    console.log(`Sarah is to young, she has to wait another ${yearsLeft} years.`)
+    console.log(`Both BMI(${markBMI}) are equal!`);
 }
-
-let birthYear = 1982;
-let century; // muss außerhalb des codeblocks definiert werden, sonst ist es außerhalb des codeblocks nicht
-             // zugreifbar, auch wenn es in beiden Schleifen definiert ist.
-
-if (birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
-
-console.log(century);
