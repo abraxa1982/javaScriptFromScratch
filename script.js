f5be60e8343ce4c3f5da81f0e75cc941ceba77fe
Meike Ziesecke <m.ziesecke@travello.de>
2023-03-24 08:08:08 +0100

    Coding Chllenge #3

--- script.js
+++ script.js
@@ -1,128 +1,32 @@
-// Coding Challenge #1
-// Mark and John are trying to compare their BMI (Body Mass Index), which is
-// calculated using the formula:
-//     BMI = mass / height ** 2 = mass / (height * height) (mass in kg
-// and height in meter).
-// Your tasks:
-//     1. Store Mark's and John's mass and height in variables
-// 2. Calculate both their BMIs using the formula (you can even implement both
-// versions)
-// 3. Create a Boolean variable 'markHigherBMI' containing information about
-// whether Mark has a higher BMI than John.
-//     Test data:
-//     § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
-// m tall.
-// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
-// m tall.
-//     GOOD LUCK 😀
-
-// Coding Challenge #2
-// Use the BMI example from Challenge #1, and the code you already wrote, and
-// improve it.
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
 //     Your tasks:
-//     1. Print a nice output to the console, saying who has the higher BMI. The message
-// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
-// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
-// BMI (28.3) is higher than John's (23.9)!"
-// Hint: Use an if/else statement 😉
-//     GOOD LUCK 😀
+//     1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+//     score, as well as multiple else-if blocks 😉
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+//     § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+// GOOD LUCK 😀
 
-
-/*// Type Conversion
-const inputYear= "1991";
-console.log(Number(inputYear)+18);
-console.log(inputYear+18);
-
-console.log(Number("Meike"));
-
-console.log(String(23), 23);
-
-//Type coercion
-console.log('I am ' + 41 + ' years old');
-console.log('I am ' + '41' + ' years old');
-// console.log('I am ' + String(41) + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' + '10' +3);
-console.log('23' * '10');
-console.log('23' /  '10');
-
-let n = '1' + 1;
-n = n - 1;
-console.log(n);*/
-
-// 5 falsy values: 0, '', undefined, null, NaN -> Sind nicht false aber werden dazu konvertiert, alles andere wird true
-/*
-
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Meike'));
-console.log(Boolean({}));
-
-//coercion passiert bei if/else und bei boolean Operators
-
-const money = 0;
-
-if (money) {
-    console.log("Don't spend it all");
+dolfinsScore = (97 + 112 + 101)/3;
+koalasScore  = (109 + 95 + 106)/3;
+if (dolfinsScore > koalasScore && dolfinsScore >= 100) {
+    console.log('dolphins won with ' + dolfinsScore + ' Points, koalas have ' + koalasScore );
+} else if (koalasScore > dolfinsScore && koalasScore >= 100) {
+    console.log('koalas won with ' + koalasScore + ' Points, dolphins have ' + dolfinsScore)
+} else if (koalasScore === dolfinsScore && koalasScore >= 100) {
+    console.log('draw is unentschieden in german')
 } else {
-    console.log("You should get a job");
-}
-
-let height; // undefined
-height = 123; //defined
-height = 0; //gibt undefined aus in der schleife, weill 0 falsy ist
-
-
-if (height) {
-    console.log('height is defined');
-} else {
-    console.log('height is undefined');
-}
-*/
-
-/*
-const age = '18';
-if (age === 18) console.log('You just became an adult(strict)');
-if (age == 18) console.log('You just became an adult(loose)');
-
-const favourite = Number(prompt("What's your favourite number?"));
-
-console.log(favourite, typeof favourite);
-
-if (favourite === 23) {
-    console.log("Cool, 23 ist an amazing number");
-} else if (favourite === 7) {
-    console.log("Cool, 7 is also an amazing number");
-} else {
-    console.log("Neither 23 nor 7");
-}
-
-if (favourite !== 23) {
-    console.log("Why not the 23?");
-}
-*/
-
-const hasDriversLicense = true;
-const hasGoodVision = true;
-
-console.log(hasDriversLicense && hasGoodVision);
-console.log(hasDriversLicense || hasGoodVision);
-console.log(!hasDriversLicense);
-
-// const shouldDrive = hasDriversLicense && hasGoodVision;
-//
-// if (shouldDrive) {
-//     console.log("Sarah is able to drive");
-// } else {
-//     console.log("Someone else should drive");
-// }
-
-const isTired = true;
-
-console.log(hasDriversLicense || hasGoodVision || isTired);
-
-if (hasDriversLicense && hasGoodVision && !isTired) {
-    console.log("Sarah is able to drive");
-} else {
-    console.log("Someone else should drive");
+    console.log('all below 100 Points in average')
 }
