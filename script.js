cbbb9a08749b0aaf911ab0fe77730771f1cef702
Meike Ziesecke <m.ziesecke@travello.de>
2023-04-19 08:03:37 +0200

    arrays

--- script.js
+++ script.js
@@ -4,49 +4,34 @@ function logger(logString) {
     console.log(logString);
 }
 
+const friends = ['Michael', 'Steven', 'Peter'];
+logger(friends);
+console.log(friends[0]);
 
-// Coding Challenge #1
-// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
-// gymnastics discipline, which works differently.
-//     Each team competes 3 times, and then the average of the 3 scores is calculated (so
-// one average score per team).
-// A team only wins if it has at least double the average score of the other team.
-//     Otherwise, no team wins!
-//     Your tasks:
-//     1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-// 2. Use the function to calculate the average for both teams
-// 3. Create a function 'checkWinner' that takes the average score of each team
-// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
-// to the console, together with the victory points, according to the rule above.
-//     Example: "Koalas win (30 vs. 13)"
-// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
-// Data 2
-// 5. Ignore draws this time
-// Test data:
-//     § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
-// Hints:
-//     § To calculate average of 3 values, add them all together and divide by 3
-// § To check if number A is at least double number B, check for A >= 2 * B.
-//     Apply this to the team's average scores �
-// GOOD LUCK �
+const years = new Array(1992, 1982, 1969, 2002, 2010, 2018);
+logger(years.length);
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3)/3;
+//letzter Inhalt
+logger(friends[friends.length-1]);
 
-function checkWinner(averageDolphins, averageKoalas) {
-    if (averageDolphins > averageKoalas *2) {
-        logger(`Dolphins win (${averageDolphins} vs. ${averageKoalas})`);
-    } else if (averageKoalas > averageDolphins *2) {
-        logger(`Koalas win (${averageKoalas} vs. ${averageDolphins})`);
-    }
+friends[2] = 'Jane';
+
+logger(friends)
+
+const calcAge = function (birthYear) {
+    return 2032 - birthYear;
 }
 
-const averageDolphins = calcAverage(44,23,71);
-const averageKoalas = calcAverage(65,54,49);
 
-checkWinner(averageDolphins, averageKoalas);
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length-1]);
 
-const averageDolphins2 = calcAverage(85,54,41);
-const averageKoalas2 = calcAverage(23,34,27);
+console.log(age1, age3, age2);
 
-checkWinner(averageDolphins2, averageKoalas2);
+const ages = [
+    calcAge(years[0]),
+    calcAge(years[1]),
+    calcAge(years[years.length-1])
+]
+console.log(ages)
