143617360e2a1cc069b1566e796c0d76b1971f36
Meike Ziesecke <m.ziesecke@travello.de>
2023-04-20 08:40:07 +0200

    array coding challenge

--- script.js
+++ script.js
@@ -1,68 +1,54 @@
-'use strict';
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+//     1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) 😉
+// GOOD LUCK 😀
 
-function logger(logString) {
-    console.log(logString);
+function calcTip(billValue) {
+    let tip;
+
+    if(billValue<=50 || billValue >= 300) {
+        tip = (billValue/100)*20
+    } else {
+        tip = (billValue/100)*15
+    }
+    return tip;
 }
 
-const friends = ['Michael', 'Steven', 'Peter'];
-// logger(friends);
-// console.log(friends[0]);
-//
-// const years = new Array(1992, 1982, 1969, 2002, 2010, 2018);
-// logger(years.length);
-//
-// //letzter Inhalt
-// logger(friends[friends.length-1]);
-//
-// friends[2] = 'Jane';
-//
-// logger(friends)
-//
-// const calcAge = function (birthYear) {
-//     return 2032 - birthYear;
-// }
-//
-//
-// const age1 = calcAge(years[0]);
-// const age2 = calcAge(years[1]);
-// const age3 = calcAge(years[years.length-1]);
-//
-// console.log(age1, age3, age2);
-//
-// const ages = [
-//     calcAge(years[0]),
-//     calcAge(years[1]),
-//     calcAge(years[years.length-1])
-// ]
-// console.log(ages)
+console.log(calcTip(100));
 
+const bills = [
+    125,
+    555,
+    44
+];
 
-// add elements
-const newLength = friends.push('Nadine');
-friends.unshift('inga');
-console.log(friends);
+const tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2])
+    ];
 
-console.log(newLength);
+console.log(tips);
 
-//remove elements
-
-const popped = friends.pop();
-console.log(friends)
-console.log(popped);
-
-friends.shift();
-console.log(friends);
-
-console.log(friends.indexOf('Steven'));
-console.log(friends.indexOf('Bob'));//<-- nicht da, ergibt -1
-
-friends.push(23);
-console.log(friends.includes('Bob'));
-console.log(friends.includes('Steven'));
-console.log(friends.includes('23'));
-
-if (friends.includes('Peter')) {
-    console.log('You have a friend called Peter');
-} else {
-    console.log('You have no friend called Peter');
-}
+const totals = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2],
+];
+console.log(totals);
