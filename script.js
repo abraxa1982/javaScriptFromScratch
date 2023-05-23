7a6ec0366a84d5263774ed3e3761f10b63fe45cf
Meike Ziesecke <m.ziesecke@travello.de>
2023-05-23 09:28:09 +0200

    game logic, duplicate code

--- script.js
+++ script.js
@@ -15,13 +15,36 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+const secretNumber = Math.trunc(Math.random() * 20)+1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check')
     .addEventListener('click', function () {
         const guess = Number(document.querySelector('.guess').value);
 
         if(!guess) {
             document.querySelector('.message').textContent = 'Please Enter an Number';
-        } else {
+        } else if(guess === secretNumber) {
             document.querySelector('.message').textContent = 'Correct Number!';
+        }else if(guess > secretNumber) {
+            if (score >1 ) {
+                document.querySelector('.message').textContent = 'Number to high';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost the Game';
+                document.querySelector('.score').textContent = 0;
+            }
+            } else {
+            if (score >1 ) {
+            document.querySelector('.message').textContent = 'Number to low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost the Game';
+                document.querySelector('.score').textContent = 0;
+            }
         }
-    })
+    });
