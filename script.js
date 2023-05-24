fe519ead0ced6888a68042c0de037f8d8b9f2ec3
Meike Ziesecke <m.ziesecke@travello.de>
2023-05-24 08:05:20 +0200

    codingChallenge

--- script.js
+++ script.js
@@ -15,19 +15,26 @@ console.log(document.querySelector('.guess').value);
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20)+1;
+let secretNumber = Math.trunc(Math.random() * 20)+1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
-
-document.querySelector('.check')
+    document.querySelector('.check')
     .addEventListener('click', function () {
         const guess = Number(document.querySelector('.guess').value);
 
         if(!guess) {
             document.querySelector('.message').textContent = 'Please Enter an Number';
         } else if(guess === secretNumber) {
+            document.querySelector('.number').textContent = secretNumber;
             document.querySelector('.message').textContent = 'Correct Number!';
+
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem'
+            if (highscore < score) {
+                highscore = score;
+                document.querySelector('.highscore').textContent = score;
+            }
         }else if(guess > secretNumber) {
             if (score >1 ) {
                 document.querySelector('.message').textContent = 'Number to high';
@@ -47,4 +54,17 @@ document.querySelector('.check')
                 document.querySelector('.score').textContent = 0;
             }
         }
+    });
+
+document.querySelector('.again')
+    .addEventListener('click', function () {
+        secretNumber = Math.trunc(Math.random() * 20)+1;
+        score = 20;
+        document.querySelector('.message').textContent = 'Start guessing...'
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.number').textContent = '?';
+        document.querySelector('.guess').value = '';
+
+        document.querySelector('body').style.backgroundColor = '#222';
+        document.querySelector('.number').style.width = '15rem'
     });
