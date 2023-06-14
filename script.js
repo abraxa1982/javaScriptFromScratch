3dc3e2c542f7649f6ee0cb338a4ee673b0d4d91e
Meike Ziesecke <m.ziesecke@travello.de>
2023-06-14 07:59:33 +0200

    Scope Übungen

--- script.js
+++ script.js
@@ -1,77 +1,38 @@
 'use strict';
 
-//Select Elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0Element = document.querySelector('#score--0');
-const score1Element = document.getElementById('score--1'); //schneller, gehen aber beide
-const current0Element = document.getElementById('current--0');
-const current1Element = document.getElementById('current--1');
-const diceElement = document.querySelector('.dice');
-const btnRoll = document.querySelector('.btn--roll');
-const btnNew = document.querySelector('.btn--new');
-const btnHold = document.querySelector('.btn--hold');
-//starting Conditions
+function calcAge(birthYear) {
+    const age =  2037 - birthYear;
 
-let scores;
-let currentScore;
-let activePlayer;
-let playing;
+    function printAge() {
+        let output = `${firstname} is ${age}, born in ${birthYear}`;
+        console.log(output)
 
-function init() {
-    scores = [0,0];
-    currentScore = 0;
-    activePlayer = 0;
-    playing = true;
-    document.querySelector(`.player--0`).classList.remove('player--winner');
-    document.querySelector(`.player--1`).classList.remove('player--winner');
-    document.getElementById(`current--0`).textContent = currentScore;
-    document.getElementById(`current--1`).textContent = currentScore;
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-    score0Element.textContent = 0;
-    score1Element.textContent = 0;
-    diceElement.classList.add('hidden');
-}
-init();
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            const firstname = 'Paula'; //Weil erst hier gesucht wird. Ist aber für mein Empfinden unsauber.
 
-function switchPlayer() {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
+            output = 'New Output!';
+            var millennial = true;
+            const str = `Oh, you're a millenial ${firstname}`;
+            console.log(str);
+
+            function add(a,b) {
+                return a+b;
+            }
+        }
+        // console.log(str);
+        console.log(millennial);
+        // console.log(add(4,5)); - würde funktionieren, wenn strict Mode aus wäre
+        console.log(output);
+    }
+
+    printAge();
+    return age
 }
 
-btnRoll.addEventListener('click', function () {
-    if (playing) {
-        const diceRoll = Math.trunc(Math.random() * 6) + 1;
-        diceElement.classList.remove('hidden');
-        diceElement.src = `dice-${diceRoll}.png`;
 
-        if (diceRoll !== 1) {
-            currentScore += diceRoll;
-            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        } else {
-            switchPlayer();
-        }
-    }
-});
 
-btnHold.addEventListener('click', function() {
-    if (playing) {
-        scores[activePlayer] += currentScore;
-        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+const firstname = 'Meike';
 
-        if (scores[activePlayer] >= 20) {
-            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-            diceElement.classList.add('hidden');
-            playing = false;
-        } else {
-            switchPlayer();
-        }
-    }
-});
-
-btnNew.addEventListener('click', init);
+calcAge(1982);
+// console.log(age);
+// printAge();
