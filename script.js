88ffbfd2cc5019698457822caaa62f690ad789a7
Meike Ziesecke <m.ziesecke@travello.de>
2023-06-16 08:17:43 +0200

    Hoistinmg and TDZ

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age =  2037 - birthYear;
+console.log(me);
+// console.log(job);
+// console.log(year);
 
-    function printAge() {
-        let output = `${firstname} is ${age}, born in ${birthYear}`;
-        console.log(output)
+console.log(addDecl(2,3));
+// console.log(addExpr(4,5));
+// console.log(addArrow(5,5));
 
-        if(birthYear >= 1981 && birthYear <= 1996) {
-            const firstname = 'Paula'; //Weil erst hier gesucht wird. Ist aber für mein Empfinden unsauber.
+var me = "Meike";
+let job = 'Developer';
+const year = 1982;
 
-            output = 'New Output!';
-            var millennial = true;
-            const str = `Oh, you're a millenial ${firstname}`;
-            console.log(str);
+function addDecl(a,b) {
+    return a+b;
+}
 
-            function add(a,b) {
-                return a+b;
-            }
-        }
-        // console.log(str);
-        console.log(millennial);
-        // console.log(add(4,5)); - würde funktionieren, wenn strict Mode aus wäre
-        console.log(output);
-    }
+const addExpr = function (a,b) {
+    return a+b;
+};
 
-    printAge();
-    return age
+const addArrow = (a,b) => a+b;
+
+
+// Example
+
+if (!numProduct) deleteShoppingCard();
+
+var numProduct = 10;
+
+function  deleteShoppingCard() {
+    console.log('All Products deleted');
 }
 
 
+//Best Practices -> don't use var, use const or let
+// Variables declaration at the top of the code
+// after that all funtions and only after all that the Rest of the code
 
-const firstname = 'Meike';
 
-calcAge(1982);
-// console.log(age);
-// printAge();
+var x = 1;
+let y= 2;
+const z = 3;
