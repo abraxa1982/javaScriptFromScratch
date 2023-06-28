8e3bfe4215b2b8bcee71803854cf54afd6c9e839
Meike Ziesecke <m.ziesecke@travello.de>
2023-06-28 07:57:12 +0200

    looping arrays and enhanced object literals

--- script.js
+++ script.js
@@ -1,88 +1,76 @@
-// We're building a football betting app (soccer for my American friends 😅)!
-// Suppose we get data from a web service about a certain game ('game' variable on
-// next page). In this challenge we're gonna work with that data.
-// Your tasks:
-//     1. Create one player array for each team (variables 'players1' and
-// 'players2')
-// 2. The first player in any player array is the goalkeeper and the others are field
-// players. For Bayern Munich (team 1) create one variable ('gk') with the
-//   goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-// field players
-// 3. Create an array 'allPlayers' containing all players of both teams (22
-// players)
-// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
-// new array ('players1Final') containing all the original team1 players plus
-// 'Thiago', 'Coutinho' and 'Perisic'
-// 5. Based on the game.odds object, create one variable for each odd (called
-// 'team1', 'draw' and 'team2')
-// 6. Write a function ('printGoals') that receives an arbitrary number of player
-// names (not an array) and prints each of them to the console, along with the
-//   number of goals that were scored in total (number of player names passed in)
-// 7. The team with the lower odd is more likely to win. Print to the console which
-// team is more likely to win, without using an if/else statement or the ternary
-//   operator.
-//       Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
-//     Then, call the function again with players from game.scored
-// GOOD LUCK 😀
+'use strict';
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-    'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const weekdays = [
+    'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'
+]
+const openingHours= {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2+4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  //ES6 Enhanced object literals -> no need to assign when it has the same name
+  openingHours,
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
+
+  //new Syntax since ES6
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  //old Syntax
+  orderDelivery: function({starterIndex =1, mainIndex = 0, time='20:00', address}) {
+    console.log(
+        `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+        will be delivered to ${address} at ${time}`);
+  },
+
+  oderPasta(ing1, ing2, ing3) {
+    console.log(`here is your delicious Pasta with ${ing1}, ${ing2} and ${ing3}.`)
+  },
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   },
 };
 
-[players1, players2] = game.players;
-[gk,...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic' ];
-console.log(players1Final);
-({team1, x:draw, team2} = game.odds);
-console.log(team1, draw, team2);
-
-function printGoals(...playerNames) {
-  console.log(playerNames);
-  console.log(`${playerNames.length} goals were scored`);
-}
-printGoals(...game.scored);
-
-team1 < team2 && console.log('Team1 is more likely to win')
-
-team1 > team2 && console.log('Team2 is more likely to win')
+console.log(restaurant);
+//
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+//
+// for (const item of menu) console.log(item);
+//
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i} : ${el}`);
+// }
